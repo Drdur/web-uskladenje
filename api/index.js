ca1f494e9db55d388d4232b2b4731dad1ef937f1
Drdur <davor@example.com>
2026-03-05 15:50:58 +0100

    Organize_API_helpers_and_static_files

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { runScan } = require('./scanner');
-const { supabase } = require('./db');
+const { runScan } = require('./_scanner');
+const { supabase } = require('./_db');
 require('dotenv').config();
 
 const app = express();
